58b8edb0f84716780a88f4e941f50e69c0c6dcb2
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-04-09 21:58:13 +0000

    adding 2 different solutions

--- descending.py
+++ descending.py
@@ -2,3 +2,18 @@ def descending_order(num):
     digits = [int(d) for d in str(num)]
     digits.sort(reverse=True)
     return int(''.join(map(str, digits)))
+
+
+# other solution
+
+def Descending_Order(num):
+    return int("".join(sorted(str(num), reverse=True)))
+
+
+# other solution
+
+def Descending_Order(num):
+    if isinstance(num, int) and num >= 0:
+        return int(''.join(sorted(str(num),reverse=True)))
+    else:
+        raise ValueError('Non-negative integer expected')
