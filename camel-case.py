5dc48041e5bd416cf9fa84d1e7a9d62ea4582ab3
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-04-10 10:05:00 +0000

    transform - to camel case

--- camel-case.py
+++ camel-case.py
@@ -2,3 +2,17 @@ import re
 
 def break_camel_case(s):
     return re.sub(r'([a-z])([A-Z])', r'\1 \2', s)
+
+
+# transform - to camel case 
+
+def to_camel_case(text):
+    return re.sub('[-_](.)', lambda x: x.group(1).upper(), text)
+
+# other solution
+
+# def to_camel_case(text):
+    # removed = text.replace('-', ' ').replace('_', ' ').split()
+    # if len(removed) == 0:
+     #   return ''
+   # return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
