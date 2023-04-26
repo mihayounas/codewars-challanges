cd6b3c98abf524c2b6570357aade5beb54d66864
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-04-26 20:49:42 +0000

    preserving functions wich will share a specific page or article

--- preserve-functions.js
+++ preserve-functions.js
@@ -7,3 +7,19 @@ const handleChange = (e) => {
     navigate('/articletopics', { state: e.target.innerText })
     setIsShow(false)
   }
+
+
+    // Construct the LinkedIn sharing URL
+    const shareUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(websiteUrl)}&title=${encodeURIComponent(websiteTitle)}`;
+  
+    // Open a new window to share the URL on LinkedIn
+    window.open(shareUrl, '_blank');
+  }
+  
+  // Share on Twitter
+  function shareOnTwitter() {
+    const articleUrl = window.location.href;
+    const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(articleUrl)}&text=${document.title}`;
+    window.open(shareUrl, '_blank');
+  }
+  
